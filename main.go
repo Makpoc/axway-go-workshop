72d99b5d42fb3433b89203ecc53ad88d8a78c524
Makpoc <makpoc@gmail.com>
2018-10-17 14:18:53 +0300

    Parse json post body

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +12,29 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome"))
 }
 
+type postMessage struct {
+	URL string `json:"url"`
+}
+
+func postParser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		log.Printf("Method %s not allowed for encoding", r.Method)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var msg postMessage
+
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		log.Printf("Failed to decode message: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Got %v", msg)
+}
+
 func main () {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -19,6 +43,8 @@ func main () {
 	log.Printf("Starting server on %s", port)
 
 	http.HandleFunc("/", welcome)
+	http.HandleFunc("/postParser", postParser)
+
 
 	log.Fatal(http.ListenAndServe(":" + port, nil))
 }

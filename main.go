f2db679d6dc38e4f02aa9e1f26b99b2633566da1
Makpoc <makpoc@gmail.com>
2018-10-17 13:58:47 +0300

    Welcome page

--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import (
 	"net/http"
 )
 
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Welcome"))
+}
+
 func main () {
 	port := "6789"
 	log.Printf("Starting server on %s", port)
+
+	http.HandleFunc("/", welcome)
+
 	log.Fatal(http.ListenAndServe(":" + port, nil))
 }

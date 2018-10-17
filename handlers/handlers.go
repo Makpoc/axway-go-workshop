36ee28ac9fdd1641ae154e81685c13f770690946
Makpoc <makpoc@gmail.com>
2018-10-17 15:44:45 +0300

    Document our code

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,3 +1,4 @@
+// Package handlers contains all handlers for the shorturl service
 package handlers
 
 import (
@@ -17,14 +18,19 @@ type response struct {
 	ShortURL    string `json:"short_url"`
 }
 
+// Handler holds the handleFunctions for the shorturl service
 type Handler struct {
+	// BaseURL is the schema://host:port/ of the service. It is used when generating the short URL
 	BaseURL string
 }
 
+// New constructs a new Handler and configures it with the provided baseURL
 func New(baseURL string) Handler {
 	return Handler{baseURL}
 }
 
+// Shorten is a handleFunc that expects a POST request with a json payload and returns a response, containing the
+// generated short URL
 func (h Handler) Shorten(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		log.Printf("Method %s not allowed for encoding", r.Method)

de40f52a6f6c55cd0dfef1bc5537069ac670a7a6
Makpoc <makpoc@gmail.com>
2018-10-19 15:13:36 +0300

    Revert "Stage 3 storage"

--- cmd/shorturl/main.go
+++ cmd/shorturl/main.go
@@ -6,8 +6,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/makpoc/axway-go-workshop/storage"
-	"github.com/makpoc/axway-go-workshop/storage/mapstore"
 	"github.com/teris-io/shortid"
 
 	"github.com/makpoc/axway-go-workshop/handlers"
@@ -16,7 +14,7 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "6789"
+		port = "8080"
 	}
 	log.Printf("Starting server on %s", port)
 
@@ -27,16 +25,9 @@ func main() {
 	}
 	shortid.SetDefault(sid)
 
-	baseURL := fmt.Sprintf("http://localhost:%s/redirect/", port)
-	store := mapstore.New()
-	handler := handlers.New(baseURL, store)
-
-	// Spawns a new goroutine
-	go storage.Clean(store)
+	handler := handlers.New(fmt.Sprintf("http://localhost:%s/", port))
 
 	http.HandleFunc("/shorten", handler.Shorten)
-	// note the trailing slash - this means match /redirect/*
-	http.HandleFunc("/redirect/", handler.Redirect)
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
